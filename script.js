4611401b085955f58c9ede50e7f60a158c424c16
Santa Catarina Clube <gabrieldefreyn8@gmail.com>
2026-04-03 00:48:13 -0300

    Refactor mobile menu functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Funcionalidade do Menu Mobile
+
 const menuToggle = document.getElementById('mobile-menu');
 const navLinks = document.querySelector('.nav-links');
 
@@ -6,7 +6,7 @@ menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
-// Fechar o menu ao clicar em um link (em dispositivos móveis)
+
 const links = document.querySelectorAll('.nav-links li a');
 links.forEach(link => {
     link.addEventListener('click', () => {
